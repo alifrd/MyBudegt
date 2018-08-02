d6d3766e034f331fc5aba2e7130ac2504e23217e
ali frd <alifrd49@gmail.com>
2018-08-02 23:11:22 +0430

    added enter event and click event

--- Js/app.js
+++ Js/app.js
@@ -1,10 +1,14 @@
 
+
+
 //		BUDGET CONTROLER
 var budgetController = (function () {
 
 
 
 })();
+
+
 
 
 
@@ -16,11 +20,25 @@ var UIController = (function () {
 
 
 
+
+
+// 		CONTROLLER
 var Controller = (function (budgetCrtl,UICrtl) {
+
+	var crtlAddItem = function() {
+		
+
+	}
 	
+	document.querySelector('.add__btn').addEventListener( 'click',crtlAddItem );
+
+	document.addEventListener('keypress', function (Event) {
+		if (Event.keyCode === 13 || Event.which === 13) { //when Press Ok
+			crtlAddItem();
+		};
+	});
 
 })(budgetController,UIController);
 
 
 
-Controller.anotherPublic();
