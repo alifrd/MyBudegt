edbffbd3c0058091e6a99752e5022bc65a3a363c
ali frd <alifrd49@gmail.com>
2018-08-03 00:49:33 +0430

    added Get element functions from input in UIController

--- Js/app.js
+++ Js/app.js
@@ -15,7 +15,27 @@ var budgetController = (function () {
 //		BUDGET CONTROLER
 var UIController = (function () {
 	
+	var DOMstrins = {
+		inputType : ".add__type",
+		inputDescription : ".add__description" ,
+		inputValue: ".add__value",
+		inputBtn: '.add__btn',
 
+	}
+
+	return{
+		getInput : function () {
+			return {
+				type :  document.querySelector(DOMstrins.inputType).value ,
+				description : document.querySelector(DOMstrins.inputDescription).value ,
+				value : document.querySelector(DOMstrins.inputValue).value
+			}	
+		},
+		getDomstrings : function () {
+			return DOMstrins;
+		}
+
+	};
 })();
 
 
@@ -25,12 +45,15 @@ var UIController = (function () {
 // 		CONTROLLER
 var Controller = (function (budgetCrtl,UICrtl) {
 
+	var DOM = UICrtl.getDomstrings();
+
 	var crtlAddItem = function() {
-		
+		var input = UICrtl.getInput();
+		console.log(input)
 
 	}
 	
-	document.querySelector('.add__btn').addEventListener( 'click',crtlAddItem );
+	document.querySelector(DOM.inputBtn).addEventListener( 'click',crtlAddItem );
 
 	document.addEventListener('keypress', function (Event) {
 		if (Event.keyCode === 13 || Event.which === 13) { //when Press Ok
