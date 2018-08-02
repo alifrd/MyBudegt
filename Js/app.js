834789e26110073b60ce5b586d1940175b22fa70
ali frd <alifrd49@gmail.com>
2018-08-02 22:50:25 +0430

    added my UI Budget And Controller Madoule

---
+++ Js/app.js
@@ -0,0 +1,26 @@
+
+//		BUDGET CONTROLER
+var budgetController = (function () {
+
+
+
+})();
+
+
+
+//		BUDGET CONTROLER
+var UIController = (function () {
+	
+
+})();
+
+
+
+var Controller = (function (budgetCrtl,UICrtl) {
+	
+
+})(budgetController,UIController);
+
+
+
+Controller.anotherPublic();
